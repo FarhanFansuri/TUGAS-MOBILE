96a0243399603ed3e5c69a7b0a5ed8ce35060d6f
Farhan Fansuri <mfarhan912345@gmail.com>
2023-04-01 19:44:34 +0700

    [UPDATE]

--- utsapp/lib/submenu.dart
+++ utsapp/lib/submenu.dart
@@ -7,89 +7,87 @@ class SubMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Submenu"),
-        backgroundColor: Colors.blueGrey[300],
-      ),
-      body: Container(
-        margin: const EdgeInsets.all(30.0),
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 30.0,
-              ),
-              Card(
-                color: Colors.blueGrey[100],
-                child: SizedBox(
-                    height: 60.0,
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(context, '/trapesium');
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          SizedBox(
-                            width: 10.0,
-                          ),
-                          Icon(
-                            Icons.shape_line,
-                            size: 30.0,
-                          ),
-                          SizedBox(
-                            width: 15.0,
-                          ),
-                          Center(
-                            child: Text(
-                              "Trapesium",
-                            ),
-                          )
-                        ],
-                      ),
-                    )),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              Card(
-                color: Colors.blueGrey[400],
-                child: SizedBox(
-                    height: 60.0,
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(context, '/tabung');
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          SizedBox(
-                            width: 10.0,
-                          ),
-                          Icon(
-                            Icons.shape_line,
-                            size: 30.0,
-                            color: Colors.white,
-                          ),
-                          SizedBox(
-                            width: 15.0,
-                          ),
-                          Center(
-                            child: Text(
-                              "Tabung",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          )
-                        ],
-                      ),
-                    )),
-              ),
-            ],
-          ),
+        appBar: AppBar(
+          title: const Text("Submenu"),
+          backgroundColor: Colors.blueGrey[300],
         ),
-      ),
-    );
+        body: Container(
+            margin: const EdgeInsets.all(30.0),
+            child: Center(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  Card(
+                    color: Colors.blueGrey[100],
+                    child: SizedBox(
+                        height: 60.0,
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/trapesium');
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: const [
+                              SizedBox(
+                                width: 10.0,
+                              ),
+                              Icon(
+                                Icons.shape_line,
+                                size: 30.0,
+                              ),
+                              SizedBox(
+                                width: 15.0,
+                              ),
+                              Center(
+                                child: Text(
+                                  "Trapesium",
+                                ),
+                              )
+                            ],
+                          ),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  Card(
+                    color: Colors.blueGrey[400],
+                    child: SizedBox(
+                        height: 60.0,
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/tabung');
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: const [
+                              SizedBox(
+                                width: 10.0,
+                              ),
+                              Icon(
+                                Icons.shape_line,
+                                size: 30.0,
+                                color: Colors.white,
+                              ),
+                              SizedBox(
+                                width: 15.0,
+                              ),
+                              Center(
+                                child: Text(
+                                  "Tabung",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              )
+                            ],
+                          ),
+                        )),
+                  ),
+                ],
+              ),
+            )));
   }
 }
